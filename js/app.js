962ce919f8c28fc59816af0e0243b47cce137de9
WillTDs <w_tye@hotmail.com>
2017-11-24 14:34:21 +0000

    total styling revamp

--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-  const $main = $('main');
+$(() => {
 
   $('a[href*="#"]').click(function(e) {
     e.preventDefault();
 
-    const target = $(this.hash);
-    if (target.length) {
-      $main.animate({
-        scrollTop: target.offset().top - ($main.offset().top - $main.scrollTop())
+    const $target = $(this.hash);
+    if ($target.length) {
+      $('html, body').animate({
+        scrollTop: $target.offset().top
       }, 800);
     }
-
   });
 
 });
